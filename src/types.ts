09f230697cd996147d52e68d3907f56e36ba48f3
Srecko Stojic <srecko.stojic@neotechsolutions.org>
2023-12-27 19:54:20 +0100

    [NO-JIRA] Remove stake amount

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export const BETTING_TYPES = ['Single', 'Double', 'Treble'];
 export const MAX_PAYOUT = 10000.0;
 export const MAX_STAKE_AMOUNT = 1499.0;
 
-export type BetType = {
+export type Bet = {
   outcomeId: string;
   eventId: string;
   odds: number;
@@ -10,15 +10,14 @@ export type BetType = {
   banker?: boolean;
 };
 
-export type SystemBetType = {
+export type BetType = {
   requiredHitCount: number;
   stakeAmountPerCombination: number;
 };
 
 export type BetSlipRequest = {
-  bets: BetType[];
-  stakeAmount?: number;
-  systemBetTypes?: SystemBetType[];
+  bets: Bet[];
+  betTypes?: BetType[];
 };
 
 export type BetSlipMaxPayoutResult = {
